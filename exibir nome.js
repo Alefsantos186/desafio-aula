c0cae48ceb0e0d8c765812568cf152817f914bff
Alefsantos186 <alefmenezes186@gmail.com>
2024-11-11 21:12:23 -0300

    Add files via upload

---
+++ exibir nome.js
@@ -0,0 +1,4 @@
+const nome = "souza";
+const idade = "22"
+console.log("olá, meu nome é",nome)
+console.log("e eu tenho",idade)
---
+++ verificar numero pn.js
@@ -0,0 +1,9 @@
+function verificarNumero(num) {
+    if (num > 0) {
+        return "Positivo";
+    } if(num < 0) {
+        return "Zero";
+    }else{
+        return "Negativo"
+    }
+}
